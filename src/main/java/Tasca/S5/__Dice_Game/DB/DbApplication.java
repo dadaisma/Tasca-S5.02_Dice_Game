5427ad8941aaea7053dcd3c1b0829b43dbceef02
Davide <infodadaisma@gmail.com>
2024-07-01 12:34:06 +0200

    start

---
+++ src/main/java/Tasca/S5/__Dice_Game/DB/DbApplication.java
@@ -0,0 +1,13 @@
+package Tasca.S5.__Dice_Game.DB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tasca/S5/__Dice_Game/DB/DbApplicationTests.java
@@ -0,0 +1,13 @@
+package Tasca.S5.__Dice_Game.DB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
